4f6af671f1d4b80b87d999718f47f1811d57e7e1
dzmchar <chernyavsky_work@mail.ru>
2018-12-12 13:45:36 +0300

    introduce and integrate user data SQL service

--- rest-service/server.js
+++ rest-service/server.js
@@ -1,15 +1,30 @@
 'use strict';
 
 const express = require('express');
+const mysql = require('mysql');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const connection = mysql.createConnection({
+  host: process.env.db_host,
+  user: process.env.db_user,
+  password: process.env.db_password
+});
+
+connection.connect(err => {
+  if (err) {
+    throw err;
+  }
+});
+
 const app = express();
+
 app.get('/', (req, res) => {
-  console.log('received request');
-  res.send('HELLO WORLD');
+  connection.query('show databases;', (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
 });
 
 app.listen(PORT, HOST);
-console.log(`Server is running on ${HOST}:${PORT}`);
